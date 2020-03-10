1526ea4fc6b9509a8e622c95e88c6c7bc910640a
birme <jonas.birme@eyevinn.se>
2020-03-10 22:23:54 +0100

    Verify that loading another VOD with start time offset 0 still works

--- spec/hlsvod_spec.js
+++ spec/hlsvod_spec.js
@@ -117,6 +117,21 @@ describe("HLSVod standalone", () => {
       done();
     });
   });
+
+  it("handles start time offset of 0 when loading after another VOD", done => {
+    mockVod1 = new HLSVod('http://mock.com/mock.m3u8', null, null, 0);
+    let mockVod2 = new HLSVod('http://mock.com/mock2.m3u8', null, null, 0);
+    mockVod1.load(mockMasterManifest, mockMediaManifest)
+    .then(() => {
+      return mockVod2.loadAfter(mockVod1, mockMasterManifest, mockMediaManifest);
+    })
+    .then(() => {
+      const seqSegments = mockVod2.getLiveMediaSequenceSegments(0);
+      expect(seqSegments['1497000'][5].discontinuity).toBe(true);
+      expect(seqSegments['1497000'][6].uri).toEqual("https://tv4play-i.akamaihd.net/i/mp4root/2018-01-26/pid200032972(3953564_,T3MP445,T3MP435,T3MP425,T3MP415,T3MP48,T3MP43,T3MP4130,).mp4.csmil/segment1_3_av.ts");
+      done();
+    });
+  });
 });
 
 describe("HLSVod after another VOD", () => {
