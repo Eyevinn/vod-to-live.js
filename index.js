249810617a74fb66f7bd919b5f9bfc470304e125
birme <jonas.birme@eyevinn.se>
2020-03-31 12:40:04 +0200

    Make sure that numbers are compared with numbers - and not strings

--- index.js
+++ index.js
@@ -439,7 +439,7 @@ class HLSVod {
           }
           segIdx++;
         } else {
-          //debug(`Pushing seq=${this.mediaSequences.length} firstSeg=${sequence[Object.keys(this.segments)[0]][0].uri}`);
+          debug(`Pushing seq=${this.mediaSequences.length} firstSeg=${sequence[Object.keys(this.segments)[0]][0].uri}, length=${sequence[Object.keys(this.segments)[0]].length}, duration=${duration} < ${this.SEQUENCE_DURATION}`);
           if (sequence[Object.keys(this.segments)[0]][0].discontinuity) {
             // If first element in the sequence is a discontinuity we need to 'skip' the following element that
             // contains the segment uri and is the actual playlist item to roll over the top.
@@ -732,7 +732,7 @@ class HLSVod {
 
     debug(`Find ${bandwidth} in ${availableBandwidths}`);
     for (let i = 0; i < availableBandwidths.length; i++) {
-      if (bandwidth <= availableBandwidths[i]) {
+      if (Number(bandwidth) <= Number(availableBandwidths[i])) {
         return availableBandwidths[i];
       }
     }
@@ -750,7 +750,7 @@ class HLSVod {
       return exactMatch;
     }
     for (let i = 0; i < availableBandwidths.length; i++) {
-      if (bandwidth <= availableBandwidths[i]) {
+      if (Number(bandwidth) <= Number(availableBandwidths[i])) {
         return availableBandwidths[i];
       }
     }
